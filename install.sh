e06da19c804eaed04b611e1847c9a966f80580e4
Kevin Yokley <kyokley2@gmail.com>
2015-10-28 08:01:17 -0500

    Setting nvim as default editor

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
