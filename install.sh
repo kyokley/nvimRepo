27ba1c747bd3268d72340ec8c1f7a15be50e42de
Kevin Yokley <kyokley2@gmail.com>
2017-10-02 17:10:22 -0500

    Assuming yes with os package installs

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ sudo chown $NON_ROOT_USER:$NON_ROOT_USER -R .git
 git submodule update --init --recursive
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim xclip aptitude
-sudo aptitude install python-pip python3-pip
+sudo apt-get install -y neovim xclip aptitude
+sudo aptitude install -y python-pip python3-pip
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
