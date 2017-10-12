72cd2423a74d68d727b33b9a3a1f97cbe0eec038
Kevin Yokley <kyokley2@gmail.com>
2017-10-11 20:17:05 -0500

    Building nvim 0.2.0 from source

--- install.sh
+++ install.sh
@@ -4,14 +4,26 @@ export NON_ROOT_USER=yokley
 sudo chown $NON_ROOT_USER:$NON_ROOT_USER -R .git
 
 git submodule update --init --recursive
-sudo add-apt-repository ppa:neovim-ppa/unstable
+#sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y neovim xclip aptitude
+sudo apt-get install -y libtool libtool-bin
+
+#sudo apt-get install -y neovim
+current_dir=$(pwd)
+wget https://github.com/neovim/neovim/archive/v0.2.0.tar.gz -O /tmp/neovim.tar.gz
+cd /tmp
+tar -xvzf /tmp/neovim.tar.gz
+cd neovim-0.2.0
+make
+sudo make install
+cd $current_dir
+
+sudo apt-get install -y xclip aptitude
 sudo aptitude install -y python-pip python3-pip
 
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --install /usr/bin/vi vi $(which nvim) 60
+sudo update-alternatives --install /usr/bin/vim vim $(which nvim) 60
+sudo update-alternatives --install /usr/bin/editor editor $(which nvim) 60
 
 sudo update-alternatives --config vi
 sudo update-alternatives --config vim
