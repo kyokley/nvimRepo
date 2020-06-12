aabe45bf14e406b66192e06005a2690ab293da83
Kevin Yokley <kyokley2@gmail.com>
2020-06-12 16:09:17 -0500

    Add exuberant tags to install

--- install/ubuntu_install.sh
+++ install/ubuntu_install.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install -y neovim xclip aptitude
 sudo apt-get install -y python-pip python3-pip
 
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev exuberant-ctags
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
