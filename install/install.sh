4c0318ea0cd481ea6eb7cd53f22be0c0835fbe1e
Kevin Yokley <kyokley2@gmail.com>
2021-09-12 13:01:32 -0500

    Add bat and fzf to install scripts

--- install/install.sh
+++ install/install.sh
@@ -7,7 +7,7 @@ git submodule update --init --recursive
 
 if [ -n $USE_PAMAC ]
 then
-    pamac install xclip neovim uctags-git npm
+    pamac install xclip neovim uctags-git npm fzf bat
 
     mkdir -p ~/.local/bin
     ln -s $(which nvim) ~/.local/bin/vim
@@ -21,7 +21,7 @@ then
     sudo apt-get update
     sudo apt-get install -y neovim xclip aptitude
     sudo apt-get install -y python-pip python3-pip
-    sudo apt-get install -y npm
+    sudo apt-get install -y npm fzf bat
 
     sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev exuberant-ctags
 
