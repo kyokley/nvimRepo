a4878baa6a136f343cdbfbb2aecdac02eca36ee5
Kevin Yokley <kyokley2@gmail.com>
2021-12-24 13:19:59 -0600

    Add default pyenv packages

--- install/install_pyenv.sh
+++ install/install_pyenv.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-PY3='3.9.7'
+PY3='3.10.0'
+
+USE_APT_GET=$(which apt-get >/dev/null 2>&1 && echo "true" || echo "false")
+
+if [ $USE_APT_GET == "true" ]
+then
+    sudo apt-get update
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+fi
 
 if [ ! -h $HOME/.pyenv ]; then
     git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
@@ -14,11 +22,27 @@ if [ ! -h $HOME/.pyenv/plugins/pyenv-update ]; then
     git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 fi
 
+if [ ! -h $HOME/.pyenv/plugins/pyenv-default-packages ]; then
+    git clone https://github.com/jawshooah/pyenv-default-packages.git $HOME/.pyenv/plugins/pyenv-default-packages
+fi
 
 export PATH="$HOME/.pyenv/bin:$PATH"
 export PYENV_VERSION="$PY3"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+cat <<EOF >> $(pyenv root)/default-packages
+pdbpp
+bpython
+wheel
+pynvim
+pyflakes
+flake8
+bandit
+jedi
+poetry
+EOF
+cat $(pyenv root)/default-packages | sort -u > $(pyenv root)/default-packages
 
 pyenv install $PY3
 
