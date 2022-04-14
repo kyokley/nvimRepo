df127003ab3307b6839dc7d3250cc25322b32ad4
Kevin Yokley <kyokley2@gmail.com>
2022-04-13 20:26:38 -0500

    Add automake

--- install/install.sh
+++ install/install.sh
@@ -25,7 +25,7 @@ then
     sudo apt-get install -y python3-pip
     sudo apt-get install -y npm fzf bat
 
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev exuberant-ctags bat
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev exuberant-ctags bat automake
 
     sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
     sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
