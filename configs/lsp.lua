b54e60453ad1bf41a1bfd3e044b5f97e52b3054a
Kevin Yokley <kyokley2@gmail.com>
2023-08-19 04:34:15 -0500

    Consolidate

--- configs/lsp.lua
+++ configs/lsp.lua
@@ -18,30 +18,14 @@ lspconfig.pylsp.setup{
     settings = {
         pylsp = {
             plugins = {
-                autopep8 = {
-                    enabled = false,
-                },
-                flake8 = {
-                    enabled = false,
-                },
-                mccabe = {
-                    enabled = false,
-                },
-                pycodestyle = {
-                    enabled = false,
-                },
-                pydocstyle = {
-                    enabled = false,
-                },
-                pyflakes = {
-                    enabled = false,
-                },
-                pylint = {
-                    enabled = false,
-                },
-                yapf = {
-                    enabled = false,
-                },
+                autopep8 = { enabled = false, },
+                flake8 = { enabled = false, },
+                mccabe = { enabled = false, },
+                pycodestyle = { enabled = false, },
+                pydocstyle = { enabled = false, },
+                pyflakes = { enabled = false, },
+                pylint = { enabled = false, },
+                yapf = { enabled = false, },
             }
         }
     }
